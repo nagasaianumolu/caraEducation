6f43936a7cc5be83952f0ccfb085df966633f6c0
nagasaianumolu <nagasaianumolu@gmail.com>
2025-10-15 23:11:31 +0530

    Updated vite config for GitHub Pages and other fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "../src/Components/NavBar/NavBar";
 import Hero from "../src/Components/Hero/Hero";
 import Programs from "../src/Components/programs/Programs"
