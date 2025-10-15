525caaa01d374e4cb08ed4624739071115a0431d
nagasaianumolu <nagasaianumolu@gmail.com>
2025-10-15 22:45:11 +0530

    Updated Video link using iframes

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,19 +1,27 @@
 import React, { useRef } from "react";
-import "./VideoPlayer.css"
-import video_player from "../../assets/video-player.mp4";
-const VideoPlayer = ({playState, setPlayState}) => {
+import "./VideoPlayer.css";
 
+const VideoPlayer = ({ playState, setPlayState }) => {
     const player = useRef(null);
+
     const closePlayer = (e) => { 
-        if(e.target === player.current) {
+        if (e.target === player.current) {
+            const videoEl = player.current.querySelector('video');
+            if (videoEl) videoEl.pause();
             setPlayState(false);
         }
     }
 
-  return (
-    <div className={`video-player ${playState ? '' : 'hide'}`} ref={player} onClick={closePlayer}>
-        <video src={video_player} autoPlay muted controls></video>
-    </div>
-  )
+    return (
+        <div className={`video-player ${playState ? '' : 'hide'}`} ref={player} onClick={closePlayer}>
+            <iframe
+                  src="https://drive.google.com/file/d/1HEO-VToMHNm6T9HM12Nkj92QtARz6721/preview"
+                  allow="autoplay"
+                  title="College Video"
+></iframe>
+
+        </div>
+    );
 };
+
 export default VideoPlayer;
